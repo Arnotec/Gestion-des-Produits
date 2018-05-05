dccdf6a464cbd8664262914adde87ab7731a5e49
TSOFACK <TSOFACK@TSOFACK-PC>
2018-05-05 10:11:49 +0400

    Premier commit Envoi des fichiers

---
+++ src/main/java/org/arnotec/GestProdApplication.java
@@ -0,0 +1,12 @@
+package org.arnotec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestProdApplication.class, args);
+	}
+}
